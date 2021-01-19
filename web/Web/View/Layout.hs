0546aa15a9a58673952f875a327cefad050f65e4
Zac Wood <zac.wood9@gmail.com>
2021-01-19 12:42:17 -0500

    web: Remove bundling in production

--- web/Web/View/Layout.hs
+++ web/Web/View/Layout.hs
@@ -29,18 +29,15 @@ defaultLayout inner = H.docTypeHtml ! A.lang "en" $ [hsx|
 
 stylesheets :: Html
 stylesheets = do
-    when isDevelopment [hsx|
+    [hsx|
         <link rel="stylesheet" href="/vendor/bootstrap.min.css"/>
         <link rel="stylesheet" href="/vendor/flatpickr.min.css"/>
         <link rel="stylesheet" href="/app.css"/>
     |]
-    when isProduction [hsx|
-        <link rel="stylesheet" href="/prod.css"/>
-    |]
 
 scripts :: Html
 scripts = do
-    when isDevelopment [hsx|
+    [hsx|
         <script id="livereload-script" src="/livereload.js"></script>
         <script src="/vendor/jquery-3.2.1.slim.min.js"></script>
         <script src="/vendor/timeago.js"></script>
@@ -50,17 +47,13 @@ scripts = do
         <script src="/helpers.js"></script>
         <script src="/vendor/morphdom-umd.min.js"></script>
     |]
-    when isProduction [hsx|
-        <script src="/prod.js"></script>
-    |]
-
 
 metaTags :: Html
 metaTags = [hsx|
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-    <meta property="og:title" content="App"/>
+    <meta property="og:title" content="Attics"/>
     <meta property="og:type" content="website"/>
-    <meta property="og:url" content="TODO"/>
-    <meta property="og:description" content="TODO"/>
+    <meta property="og:url" content="https://attics.io"/>
+    <meta property="og:description" content="Stream thousands of live concerts"/>
 |]
