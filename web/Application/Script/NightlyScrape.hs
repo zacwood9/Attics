eb4fe69a5ad82fe20a3bf8dd840b6f764d03b36b
Zac Wood <zac.wood9@gmail.com>
2021-02-01 16:34:26 -0500

    fix script

--- web/Application/Script/NightlyScrape.hs
+++ web/Application/Script/NightlyScrape.hs
@@ -1,16 +1,15 @@
 #!/usr/bin/env run-script
 
-{-# LANGUAGE GeneralisedNewtypeDeriving #-}
-
 module Application.Script.NightlyScrape where
 
 import Application.Script.Prelude
+import Control.Monad (void)
 
 run :: Script
 run = do
     bands <- query @Band |> fetch
     forEach bands $ \band ->
-        newRecord @NightlyScrapeJob
+        void $ newRecord @NightlyScrapeJob
             |> set #bandId (get #id band)
             |> createRecord
 
