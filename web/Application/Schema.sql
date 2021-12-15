8ce06cd3453702c96fbb78e9b62418555234976c
Zachary Wood <zac.wood9@gmail.com>
2021-12-15 17:34:59 -0500

    fix field order

--- web/Application/Schema.sql
+++ web/Application/Schema.sql
@@ -41,37 +41,37 @@ CREATE TABLE nightly_scrape_jobs (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
-    run_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
     status JOB_STATUS DEFAULT 'job_status_not_started' NOT NULL,
     last_error TEXT DEFAULT NULL,
     attempts_count INT DEFAULT 0 NOT NULL,
     locked_at TIMESTAMP WITH TIME ZONE DEFAULT NULL,
     locked_by UUID DEFAULT NULL,
-    band_id UUID NOT NULL
+    band_id UUID NOT NULL,
+    run_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL
 );
 CREATE TABLE fix_song_jobs (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
-    run_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
     status JOB_STATUS DEFAULT 'job_status_not_started' NOT NULL,
     last_error TEXT DEFAULT NULL,
     attempts_count INT DEFAULT 0 NOT NULL,
     locked_at TIMESTAMP WITH TIME ZONE DEFAULT NULL,
     locked_by UUID DEFAULT NULL,
-    band_id UUID NOT NULL
+    band_id UUID NOT NULL,
+    run_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL
 );
 CREATE TABLE initial_scrape_jobs (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
-    run_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
     status JOB_STATUS DEFAULT 'job_status_not_started' NOT NULL,
     last_error TEXT DEFAULT NULL,
     attempts_count INT DEFAULT 0 NOT NULL,
     locked_at TIMESTAMP WITH TIME ZONE DEFAULT NULL,
     locked_by UUID DEFAULT NULL,
-    band_id UUID NOT NULL
+    band_id UUID NOT NULL,
+    run_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL
 );
 ALTER TABLE fix_song_jobs ADD CONSTRAINT fix_song_jobs_ref_band_id FOREIGN KEY (band_id) REFERENCES bands (id) ON DELETE CASCADE;
 ALTER TABLE initial_scrape_jobs ADD CONSTRAINT initial_scrape_jobs_ref_band_id FOREIGN KEY (band_id) REFERENCES bands (id) ON DELETE CASCADE;
