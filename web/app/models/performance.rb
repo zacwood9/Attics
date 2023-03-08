a7bda75de0bffe7b5c5982f9acb3d3699ef394fe
Zachary Wood <zachary.wood@appfolio.com>
2023-03-07 19:54:28 -0800

    web: handle performances with no recordings

--- web/app/models/performance.rb
+++ web/app/models/performance.rb
@@ -7,8 +7,8 @@ class Performance < ApplicationRecord
   scope :with_recording_metadata, -> {
     select('performances.*')
      .select('count(distinct recordings.id) as num_recordings')
-     .select('sum(recordings.avg_rating * recordings.num_reviews) as num_stars')
-     .select('sum(recordings.num_reviews) as num_reviews')
+     .select('COALESCE(sum(recordings.avg_rating * recordings.num_reviews), 0) as num_stars')
+     .select('COALESCE(sum(recordings.num_reviews), 0) as num_reviews')
      .left_joins(:recordings)
      .group('performances.id')
   }
