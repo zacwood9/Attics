dffc544c0b03fd619b8e962613f6e58ecaecd242
Zachary Wood <zachary.wood@appfolio.com>
2023-11-05 16:17:56 -0800

    web: delete dead code

--- web/app/models/types.rb
+++
@@ -1,8 +0,0 @@
-class Types
-  include Dry.Types()
-
-  Date = Strict::Date | JSON::Date
-  Symbol = Strict::Symbol | JSON::Symbol
-  Time = Strict::Time | JSON::Time
-  Decimal = Coercible::Decimal
-end
