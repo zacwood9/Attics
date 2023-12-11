f29b27a84024627609c591afc4dffc352e79249c
Zachary Wood <zachary.wood@appfolio.com>
2023-12-11 10:31:30 -0800

    web: match any string for collection

--- web/config/routes.rb
+++ web/config/routes.rb
@@ -15,16 +15,20 @@ Rails.application.routes.draw do
       get "/bands", to: "api/bands#index", as: :api_bands
       get "/bands/:collection/performances/top",
           to: "api/performances#top",
-          as: :api_band_top_performances
+          as: :api_band_top_performances,
+          collection: /.*/
       get "/bands/:collection/performances/on_this_day",
           to: "api/performances#top",
-          as: :api_band_on_this_day_performances
+          as: :api_band_on_this_day_performances,
+          collection: /.*/
       get "/bands/:collection/performances",
           to: "api/performances#index",
-          as: :api_band_performances
+          as: :api_band_performances,
+          collection: /.*/
       get "/bands/:collection/performances/:date/recordings",
           to: "api/recordings#index",
-          as: :api_performance_recordings
+          as: :api_performance_recordings,
+          collection: /.*/
       get "/recordings/:identifier/tracks",
           to: "api/tracks#index",
           as: :api_recording_tracks,
